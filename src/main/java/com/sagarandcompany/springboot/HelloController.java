339de1fac00ab33b6b6c6ecdbc4b36f9c42d094f
Sagar <sagarmal624@gmail.com>
2017-11-19 19:06:26 +0530

    added controler

---
+++ src/main/java/com/sagarandcompany/springboot/HelloController.java
@@ -0,0 +1,14 @@
+package com.sagarandcompany.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("name", "SagarAndCompany");
+        return "index";
+    }
+}
